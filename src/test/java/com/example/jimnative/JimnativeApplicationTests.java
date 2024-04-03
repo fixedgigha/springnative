43f5ed08ae918fe8b4e0c7597e6a30746c444561
Jim Speakman <jim@jimspeakman.net>
2024-04-03 19:05:28 +0100

    adding test

--- src/test/java/com/example/jimnative/JimnativeApplicationTests.java
+++ src/test/java/com/example/jimnative/JimnativeApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.jimnative;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class JimnativeApplicationTests {
+
+	@Autowired
+	WebTestClient webTestClient;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getExistingEmployee() {
+		webTestClient.get()
+				.uri("/employees/1")
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody().jsonPath("name").isEqualTo("reactive bob");
 	}
 
 }
